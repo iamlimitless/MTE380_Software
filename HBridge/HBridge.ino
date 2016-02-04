514ad180399c9bce9a154f5d404b5b0060c03338
Matt Coppola <macoppol@uwaterloo.ca>
2016-02-03 22:40:02 -0500

    Updated to run two motors simultaneously, Some Skeleton code in their for controlling the start and stop using IR

--- HBridge/HBridge.ino
+++ HBridge/HBridge.ino
@@ -10,40 +10,55 @@ void setup()
 {
   pinMode(AIN1_P5, OUTPUT);
   pinMode(AIN2_P6, OUTPUT);
-  /*
   pinMode(BIN1_P10, OUTPUT);
   pinMode(BIN2_P11, OUTPUT);
-  */
 }
 
-void pwmOutputMotorA(unsigned int frequency, float percentDuty)
+void pwmToMotor(unsigned int frequency, float percentDutyA, float percentDutyB)
 {
-  if(percentDuty > 1) 
-  {
-    percentDuty = percentDuty / 100.0;
-  }
+    // 1000ms/ (freq(in herz) / 1000)
+    int time = 1000000 / frequency;
 
-  // 1000ms/ (freq(in herz) / 1000)
-  int time = 1000000 / frequency;
-  unsigned int delayOn = percentDuty * time;
-  unsigned int delayOff = time - percentDuty * time;
+    if(percentDutyA > percentDutyB)
+    {
+        unsigned int firstDelay = percentDutyB * time;
+        unsigned int secondDelay = (percentDutyA * time) - firstDelay;
+        unsigned int timeRemaining = (time - firstDelay) - secondDelay;
 
-  while(true)
-  {
-    digitalWrite(AIN1_P5, HIGH);
-    delayMicroseconds(delayOn);
-    digitalWrite(AIN1_P5, LOW);
-    delayMicroseconds(delayOff);    
-  }
+        //TODO Consider making this a function. Is the overhead worth the reuse?
+        digitalWrite(AIN1_P5, HIGH);
+        digitalWrite(BIN1_P10, HIGH);
+        delayMicroseconds(firstDelay);
+        digitalWrite(BIN1_P10, LOW);
+        delayMicroseconds(secondDelay);    
+        digitalWrite(AIN1_P5, LOW);
+        delayMicroseconds(timeRemaining);
+    }
+    else //PercentB >= PercentA
+    {
+        unsigned int firstDelay = percentDutyA * time;
+        unsigned int secondDelay = (percentDutyB * time) - firstDelay;
+        unsigned int timeRemaining = (time - firstDelay) - secondDelay;
+
+        //TODO Consider making this a function. Is the overhead worth the reuse?
+        digitalWrite(AIN1_P5, HIGH);
+        digitalWrite(BIN1_P10, HIGH);
+        delayMicroseconds(firstDelay);
+        digitalWrite(AIN1_P5, LOW);
+        delayMicroseconds(secondDelay); 
+        digitalWrite(BIN1_P10, LOW);
+        delayMicroseconds(timeRemaining);
+    }
 }
 
-void driveMotorA(unsigned int frequency, float percentDuty)
+void driveForward(unsigned int frequency, float percentDutyA, float percentDutyB)
 {
   digitalWrite(AIN2_P6, LOW); 
-  pwmOutputMotorA(frequency, percentDuty);
+  digitalWrite(BIN2_P11, LOW); 
+  pwmToMotor(frequency, percentDutyA, percentDutyB);
 }
 
 void loop() 
 {
-  driveMotorA(1000, 0.95);
+  driveForward(1000, 0.5, 0.5);
 }
